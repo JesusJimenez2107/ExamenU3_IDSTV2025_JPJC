4aedbb39f8f0a83fe22bc212072eaa022cbb3cec
JesusJimenez2107 <jjimenez_23@alu.uabcs.mx>
2025-05-29 02:31:48 -0700

    prueba repo

---
+++ src/Examen_U3/Ventana.java
@@ -0,0 +1,35 @@
+package Examen_U3;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+public class Ventana extends JFrame{
+
+	public Ventana(){
+		
+	}
+	
+
+}
